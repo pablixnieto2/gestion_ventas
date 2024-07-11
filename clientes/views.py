2e10c59ac04e630c648603653148db413fdb7238
infographycs <idealesnieto@gmail.com>
2024-07-11 04:33:29 +0200

    Initial project structure913

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Cliente
+from .forms import ClienteForm
 
 def cliente_list(request):
     clientes = Cliente.objects.all()
-    return render(request, 'clientes/cliente_list.html', {'clientes': clientes})
+    return render(request, 'clientes/listar_cliente.html', {'clientes': clientes})
 
 def cliente_detail(request, id):
     cliente = get_object_or_404(Cliente, id=id)
@@ -11,11 +12,10 @@ def cliente_detail(request, id):
 
 def crear_cliente(request):
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        apellido = request.POST.get('apellido')
-        email = request.POST.get('email')
-        telefono = request.POST.get('telefono')
-        fecha_fiesta = request.POST.get('fecha_fiesta')
-        Cliente.objects.create(nombre=nombre, apellido=apellido, email=email, telefono=telefono, fecha_fiesta=fecha_fiesta)
-        return redirect('cliente_list')
-    return render(request, 'clientes/crear_cliente.html')
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('cliente_list')
+    else:
+        form = ClienteForm()
+    return render(request, 'clientes/crear_cliente.html', {'form': form})
--- gestion_ventas/urls.py
+++ gestion_ventas/urls.py
@@ -1,12 +1,13 @@
-# gestion_ventas/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
-    path('ventas/', include('ventas.urls')),
     path('productos/', include('productos.urls')),
+    path('pagos/', include('pagos.urls')),
     path('pedidos/', include('pedidos.urls')),
     path('sesiones_fotos/', include('sesiones_fotos.urls')),
+    path('ventas/', include('ventas.urls')),
+    path('', include('home.urls')),  # Assuming you have a home app for the landing page
 ]
