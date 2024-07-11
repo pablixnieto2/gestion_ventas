eea1a489eb6c7f84a0057f0528c13744a0b033f6
infographycs <idealesnieto@gmail.com>
2024-07-11 04:19:54 +0200

    Initial project structure912

--- core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Configuracion(models.Model):
-    clave = models.CharField(maxlength=50, unique=True)
-    valor = models.CharField(maxlength=255)
-
-    def __str__(self):
-        return f"{self.clave}: {self.valor}"
--- core/utils.py
+++
@@ -1,9 +0,0 @@
-def calcular_descuento(precio, porcentaje):
-    return precio - (precio * (porcentaje / 100))
-
-def generar_codigo_unico(model, campo):
-    import uuid
-    codigo = str(uuid.uuid4())[:6].upper()
-    while model.objects.filter(**{campo: codigo}).exists():
-        codigo = str(uuid.uuid4())[:6].upper()
-    return codigo
